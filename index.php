9af82906b8d89e8e6ca4bd624ca47c21a21d0010
jaycee808 <jamie.clark@live.co.uk>
2023-05-20 14:29:39 +0100

    updated search results page and search tabs

--- index.php
+++ index.php
@@ -5,20 +5,18 @@
     <title>orbit</title>
 </head>
 <body>
-    <div class="home-page">
-        <div class="header-section">
-            <div id="logo">
-                <h1>orbit</h1>
-            </div>
-            <!-- <div id="tagline">
-                <h2>search a universe of information</h2>
-            </div> -->
-            <div id="search-bar">
-                <form action="search.php" method="GET">
-                    <input class="search-box" type="text" name="term" placeholder="Let's explore">
-                    <input class="search-button" type="submit" value="Search">
-                </form>
-            </div>
+    <div id="home-page">
+        <div id="logo">
+            <h1>orbit</h1>
+        </div>
+        <div id="tagline">
+            <h2>search a universe of information</h2>
+        </div>
+        <div id="search-bar">
+            <form action="search.php" method="GET">
+                <input class="search-box" type="text" name="term" placeholder="Let's explore">
+                <input class="search-button" type="submit" value="Search">
+            </form>
         </div>
     </div>
 </body>
