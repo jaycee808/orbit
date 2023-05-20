518ff423c18853800c9de54c05281c1b3153661c
jaycee808 <jamie.clark@live.co.uk>
2023-05-20 11:13:13 +0100

    added styling to home page

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
     <title>orbit</title>
 </head>
 <body>
-    <div class="header-section">
-        <div id="logo">
-            <h1>orbit</h1>
-        </div>
-        <div id="tagline">
-            <h2>search a universe of information</h2>
-        </div>
-        <div id="search-section">
-            <form action="search.php" method="GET">
-                <input class="search-box" type="text" name="term" placeholder="Let's explore">
-                <input class="search-button" type="submit" value="Search">
-            </form>
+    <div class="home-page">
+        <div class="header-section">
+            <div id="logo">
+                <h1>orbit</h1>
+            </div>
+            <!-- <div id="tagline">
+                <h2>search a universe of information</h2>
+            </div> -->
+            <div id="search-bar">
+                <form action="search.php" method="GET">
+                    <input class="search-box" type="text" name="term" placeholder="Let's explore">
+                    <input class="search-button" type="submit" value="Search">
+                </form>
+            </div>
         </div>
     </div>
 </body>
