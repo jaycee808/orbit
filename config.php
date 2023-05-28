547c41c7f370e9bd2ea91a4ff3c43f68645a8b3c
jaycee808 <jamie.clark@live.co.uk>
2023-05-28 18:11:47 +0100

    added composer.lock file

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 ob_start();
 
 $db_host = "eu-cdbr-west-03.cleardb.net";
-$db_user = "b2ccc9f753b5ac";
-$db_pass = "889c3d10";
-$db_name = "heroku_f00d3f311846201";
+$db_user = "bb8580a4c8f2f1";
+$db_pass = "2a529bca";
+$db_name = "heroku_0a1ecdf3cfac680";
 
 try {
     $connection = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
