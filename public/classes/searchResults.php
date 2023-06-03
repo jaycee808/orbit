4ee44b1739fa8127f3f4fbaff98431b80918e0dd
jaycee808 <jamie.clark@live.co.uk>
2023-06-03 13:20:15 +0100

    added function and variables to trim returned data

--- public/classes/searchResults.php
+++ public/classes/searchResults.php
@@ -1,85 +1,98 @@
 <?php
 class searchResults {
 
-	private $connection;
+    private $connection;
 
-	public function __construct($connection) {
-		$this->connection = $connection;
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+
+    public function getNumResults($term) {
+
+        $query = $this->connection->prepare("SELECT COUNT(*) as total 
+                                    FROM pages WHERE title LIKE :term 
+                                    OR url LIKE :term 
+                                    OR keywords LIKE :term 
+                                    OR description LIKE :term");
+
+        $searchTerm = "%" . $term . "%";
+        $query->bindParam(":term", $searchTerm);
+        $query->execute();
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        return $row["total"];
+    }
+
+    public function resultsPages($pageIndex, $numOfResultsPerPage, $term, $maxPages) {
+
+        $totalResults = $this->getNumResults($term);
+        $totalPages = ceil($totalResults / $numOfResultsPerPage);
+        
+        $pageIndex = max(min($pageIndex, $totalPages), 1);
+        $startPage = max(min($pageIndex - floor($maxPages / 2), $totalPages - $maxPages + 1), 1);
+        $endPage = min($startPage + $maxPages - 1, $totalPages);
+        
+        $displayResults = ($pageIndex - 1) * $numOfResultsPerPage;
+
+        $query = $this->connection->prepare("SELECT * 
+                                    FROM pages WHERE title LIKE :term 
+                                    OR url LIKE :term 
+                                    OR keywords LIKE :term 
+                                    OR description LIKE :term
+                                    ORDER BY clicks DESC
+                                    LIMIT :displayResults, :numOfResults");
+
+        $searchTerm = "%" . $term . "%";
+        $query->bindParam(":term", $searchTerm);
+        $query->bindParam(":displayResults", $displayResults, PDO::PARAM_INT);
+        $query->bindParam(":numOfResults", $numOfResultsPerPage, PDO::PARAM_INT);
+        $query->execute();
+        
+        // Add total number of results
+        $resultsHtml = "<div class='totalResults'>Total results: $totalResults</div>";
+
+        $resultsHtml .= "<div class='searchResults'>";
+
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $id = $row["id"];
+            $url = $row["url"];
+            $title = $row["title"];
+            $description = $row["description"];
+            
+            // trim variable data
+			$url = $this->trimField($url, 35);
+			$description = $this->trimField($description, 500);
+
+            $resultsHtml .=
+                "<div class='resultContainer'>
+                    <div class='resultDetail'>
+                        <h3 class='title'>
+                        <a class='result' href='$url'>
+                        $title
+                        </a>
+                        </h3>
+                        <span class='url'>$url</span><br />
+                        <span class='description'>$description</span>
+                    </div>
+                </div>";
+        }
+
+        $resultsHtml .= "</div>";
+
+        // Add page links
+        $resultsHtml .= "<div class='pagination'>";
+        for ($page = $startPage; $page <= $endPage; $page++) {
+            $resultsHtml .= "<a href='search.php?term=$term&page=$page'>$page</a>";
+        }
+        $resultsHtml .= "</div>";
+
+        return $resultsHtml;
+    }
+
+    private function trimField($string, $characterLimit) {
+
+		$dots = strlen($string) > $characterLimit ? "..." : "";
+		return substr($string, 0, $characterLimit) . $dots;
 	}
-
-	public function getNumResults($term) {
-
-		$query = $this->connection->prepare("SELECT COUNT(*) as total 
-									FROM pages WHERE title LIKE :term 
-									OR url LIKE :term 
-									OR keywords LIKE :term 
-									OR description LIKE :term");
-
-		$searchTerm = "%". $term . "%";
-		$query->bindParam(":term", $searchTerm);
-		$query->execute();
-
-		$row = $query->fetch(PDO::FETCH_ASSOC);
-		return $row["total"];
-	}
-
-	public function resultsPages($pageIndex, $numOfResultsPerPage, $term) {
-
-		$totalResults = $this->getNumResults($term);
-		$totalPages = ceil($totalResults / $numOfResultsPerPage);
-	
-		$displayResults = ($pageIndex - 1) * $numOfResultsPerPage;
-	
-		$query = $this->connection->prepare("SELECT * 
-									FROM pages WHERE title LIKE :term 
-									OR url LIKE :term 
-									OR keywords LIKE :term 
-									OR description LIKE :term
-									ORDER BY clicks DESC
-									LIMIT :displayResults, :numOfResults");
-	
-		$searchTerm = "%" . $term . "%";
-		$query->bindParam(":term", $searchTerm);
-		$query->bindParam(":displayResults", $displayResults, PDO::PARAM_INT);
-		$query->bindParam(":numOfResults", $numOfResultsPerPage, PDO::PARAM_INT);
-		$query->execute();
-		
-					// Add total number of results
-		$resultsHtml = "<div class='totalResults'>Total results: $totalResults</div>";
-
-		$resultsHtml .= "<div class='searchResults'>";
-	
-		while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-			$id = $row["id"];
-			$url = $row["url"];
-			$title = $row["title"];
-			$description = $row["description"];
-			
-			$resultsHtml .=
-				"<div class='resultContainer'>
-					<div class='resultDetail'>
-						<h3 class='title'>
-						<a class='result' href='$url'>
-						$title
-						</a>
-						</h3>
-						<span class='url'>$url</span><br />
-						<span class='description'>$description</span>
-					</div>
-				</div>";
-		}
-	
-		$resultsHtml .= "</div>";
-
-		// Add page links
-		$resultsHtml .= "<div class='pagination'>";
-		for ($page = 1; $page <= $totalPages; $page++) {
-			$resultsHtml .= "<a href='search.php?term=$term&page=$page'>$page</a>";
-		}
-		$resultsHtml .= "</div>";
-	
-		return $resultsHtml;
-	}
-	
 }
 ?>
--- public/search.php
+++ public/search.php
@@ -1,18 +1,17 @@
 <?php
 include("./classes/searchResults.php");
 include("config.php");
-// php code for highlighting active search tab
 
-	$type = isset($_GET["type"]) ? $_GET["type"] : "pages";
-    $pageIndex = isset($_GET["pageIndex"]) ? $_GET["pageIndex"] : 1;
+$type = isset($_GET["type"]) ? $_GET["type"] : "pages";
+$pageIndex = isset($_GET["pageIndex"]) ? $_GET["pageIndex"] : 1;
 
-    if(isset($_GET["term"])) {
-        $term = $_GET["term"];
-    } else {
+if(isset($_GET["term"])) {
+    $term = $_GET["term"];
+} else {
     exit("Please enter a search value");
-    }
+}
 
-    // echo $_GET["term"];
+// echo $_GET["term"];
 ?>
 
 <!DOCTYPE html>
@@ -22,49 +21,44 @@ include("config.php");
     <title>orbit</title>
 </head>
 <body>
-
-<!-- header with logo and search bar -->
-<div id="search-page">
-    <div class="search-page-header">
-        <div id="logo">
-            <a href="./index.php">
-            <h1>orbit</h1>
-            </a>
-        </div>
-    
-    <div id="search-bar">
-        <form action="search.php" method="GET">
-            <input class="search-box" type="text" name="term" placeholder="<?php echo $term ?>">
-            <input class="search-button" type="submit" value="Search">
-        </form>
-    </div>
-</div>
-
-    <!-- tabs to filter search results -->
-    <div class="search-tabs">
-        <ul class="tab-list">
-            <li class="<?php echo $type == 'pages' ? 'active' : '' ?>">
-                <a href='<?php echo "search.php?term=$term&type=pages"; ?>'>
-                Pages
+    <!-- header with logo and search bar -->
+    <div id="search-page">
+        <div class="search-page-header">
+            <div id="logo">
+                <a href="./index.php">
+                    <h1>orbit</h1>
                 </a>
-            </li>
-        </ul>
+            </div>
+            <div id="search-bar">
+                <form action="search.php" method="GET">
+                    <input class="search-box" type="text" name="term" placeholder="<?php echo $term ?>">
+                    <input class="search-button" type="submit" value="Search">
+                </form>
+            </div>
+        </div>
+
+        <!-- tabs to filter search results -->
+        <div class="search-tabs">
+            <ul class="tab-list">
+                <li class="<?php echo $type == 'pages' ? 'active' : '' ?>">
+                    <a href='<?php echo "search.php?term=$term&type=pages"; ?>'>
+                        Pages
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
-</div>
 
     <!-- section to display search results -->
     <div id="resultsDisplay">
-
-    <?php
+        <?php
         $search = new searchResults($connection);
-
         $pageIndex = isset($_GET['page']) ? intval($_GET['page']) : 1;
         $numOfResultsPerPage = 20;
-        
-        echo $search->resultsPages($pageIndex, $numOfResultsPerPage, $term);
-    ?>
+        $maxPages = 6;
+
+        echo $search->resultsPages($pageIndex, $numOfResultsPerPage, $term, $maxPages);
+        ?>
     </div>
-
-
 </body>
 </html>
