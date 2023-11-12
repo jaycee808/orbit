3e28696c136a0d9392a3b338d8e9b0cb352182f5
jaycee808 <jamie.clark@live.co.uk>
2023-11-12 20:56:28 +0000

    updated font sizes

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
 </head>
 <body>
-    <div id="home-page">
-        <header class="logo">orbit</header>
+    <section id="home-page">
+        <div class="logo">orbit</div>
             <div class="tagline">
                 search a universe of information
             </div>
@@ -25,6 +25,6 @@
                     <li><a href="search.php?term=travel">Travel</a></li>
                 </ul>
             </div>
-    </div>
+    </section>
 </body>
 </html>
