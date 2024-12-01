a3e037c002b596e464a93c648ec29034e446e837
jaycee808 <jamie.clark@live.co.uk>
2024-12-01 11:45:29 +0000

    updated database details

--- public/config.php
+++ public/config.php
@@ -1,10 +1,10 @@
 <?php
 ob_start();
 
-$db_host = "eu-cluster-west-01.k8s.cleardb.net";
-$db_user = "b0a8b12ba113bc";
-$db_pass = "802fce8a";
-$db_name = "heroku_9f87f24fd978d58";
+$host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
+$dbname = getenv('MYSQLDATABASE') ?: 'railway';
+$username = getenv('MYSQLUSER') ?: 'root';
+$password = getenv('MYSQLPASSWORD') ?: 'AUCJPTPtNrSvaBaHlJERimcxKKDFcjrr';
 
 try {
     $connection = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
